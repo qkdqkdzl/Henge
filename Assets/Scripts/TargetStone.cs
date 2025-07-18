b97f99ef86c7e8f24356a5b2c3d6f2057593d0f8
Hammysarone <107188468+Hammysarone@users.noreply.github.com>
2025-07-18 21:50:00 +0900

    스톤 이미지 추가

--- Assets/Scripts/TargetStone.cs
+++ Assets/Scripts/TargetStone.cs
@@ -13,6 +13,7 @@ public class TargetStone : MonoBehaviour
     public static event Action<StoneType> OnHitByProjectile;
     public static event Action<StoneType> OnKnockDownEvent;
     public static event Action<Vector3> OnKnockDownToZombiEvent;
+    
     public StoneType stoneType;
     public Renderer objRenderer;
 
@@ -26,23 +27,44 @@ public class TargetStone : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
         if (collision.gameObject.CompareTag("Stone"))
         {
             OnHitByProjectile?.Invoke(stoneType);
             isHit = true;
         }
+        ContactPoint contact = collision.contacts[0];
+        Vector3 contactPoint = contact.point;
+
+
+        if (meshCollider != null)
+        {
+            float edgeDsitance = EdgeDistanceCalculator.GetDistanceToNearestEdge(meshCollider, contactPoint);
+            Debug.Log("Distanec to nearest edge: " + edgeDsitance);
+        }
+        
+
     }
+
+
 
     void Update()
     {
         if (isHit)
         {
+            //TODO : 넘어짐 판정 수정
+            //과제:   타켓 스톤의 넘어짐 판정을  다시 만들기 
+            //울퉁불퉁한 돌이 넘어지면, 270도, 90도가 아니어서,
+            //기존 코드는 완벽하지 않음.그래서, 이 부분을 레이케스트를 이용해서 넘어짐 판정을 하는 것으로 코딩하기.
+
+            
 
             if ((Math.Abs(transform.eulerAngles.z - 270) < 3f) || Math.Abs(transform.eulerAngles.z - 90f) < 3)
             {
                 OnKnockDownToZombiEvent?.Invoke(transform.position);
                 StartCoroutine(FadeOutObject());
                 isHit = false;
+
             }
         }
     }
